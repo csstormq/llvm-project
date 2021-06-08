b09466720bfb6239405024a5b060f1c565cc48b0
xuxiaoqiang <swust_xiaoqiangxu@163.com>
2021-06-08 15:47:07 +0800

    add checkDeadSymbols to clear unused state

--- clang/lib/Analysis/plugins/CheckerSimpleStream/SimpleStreamChecker.cpp
+++ clang/lib/Analysis/plugins/CheckerSimpleStream/SimpleStreamChecker.cpp
@@ -9,19 +9,20 @@ using namespace ento;
 
 namespace {
 
-// Kind of stream API status
 enum Kind {
   Opened,
   Closed,
 };
 
-class SimpleStreamChecker : public Checker<check::PostCall> {
+class SimpleStreamChecker : public Checker<check::PostCall,
+                                           check::DeadSymbols> {
   mutable std::unique_ptr<BugType> DoubleCloseBT;
 
   void reportDoubleClose(CheckerContext &C) const;
 
 public:
   void checkPostCall(const CallEvent &Call, CheckerContext &C) const;
+  void checkDeadSymbols(SymbolReaper &SR, CheckerContext &C) const;
 };
 
 } // anonymous namespace
@@ -48,37 +49,53 @@ void SimpleStreamChecker::checkPostCall(const CallEvent &Call,
     ProgramStateRef State = C.getState();
     State = State->set<SimpleStreamMap>(FileDesc, Opened);
     C.addTransition(State);
+    return;
   }
-  else if (II->isStr("fclose")) {
+
+  if (II->isStr("fclose")) {
     SymbolRef FileDesc = Call.getArgSVal(0).getAsSymbol();
     if (!FileDesc) {
       return;
     }
 
     ProgramStateRef State = C.getState();
-    const auto Status = State->get<SimpleStreamMap>(FileDesc);
-    if (!Status) {
-      return;
+    if (const auto K = State->get<SimpleStreamMap>(FileDesc)) {
+      if (*K == Opened) {
+        State = State->set<SimpleStreamMap>(FileDesc, Closed);
+        C.addTransition(State);
+      }
+      else if (*K == Closed) {
+        reportDoubleClose(C);
+      }
     }
-    if (*Status == Opened) {
-      State = State->set<SimpleStreamMap>(FileDesc, Closed);
-      C.addTransition(State);
-    }
-    else if (*Status == Closed) {
-      reportDoubleClose(C);
-    }
+    return;
   }
 }
 
 void SimpleStreamChecker::reportDoubleClose(CheckerContext &C) const {
   if (!DoubleCloseBT) {
     DoubleCloseBT.reset(
-      new BugType(this, "Double fclose","Unix Stream API Error"));
+      new BugType(this, "Double fclose", "Unix Stream API Error"));
   }
-  ExplodedNode *N = C.generateErrorNode();
-  auto R = std::make_unique<PathSensitiveBugReport>(
-    *DoubleCloseBT, DoubleCloseBT->getDescription(), N);
-  C.emitReport(std::move(R));
+
+  if (ExplodedNode *N = C.generateErrorNode()) {
+    auto R = std::make_unique<PathSensitiveBugReport>(
+      *DoubleCloseBT, DoubleCloseBT->getDescription(), N);
+    C.emitReport(std::move(R));
+  }
+}
+
+void SimpleStreamChecker::checkDeadSymbols(SymbolReaper &SR,
+                                           CheckerContext &C) const {
+  ProgramStateRef State = C.getState();
+  for (const auto &TrackedStream : State->get<SimpleStreamMap>()) {
+    SymbolRef Sym = TrackedStream.first;
+    if (SR.isDead(Sym)) {
+      State = State->remove<SimpleStreamMap>(Sym);
+    }
+  }
+
+  C.addTransition(State);
 }
 
 // Register plugin!
