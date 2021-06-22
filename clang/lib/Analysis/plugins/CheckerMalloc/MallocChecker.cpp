3487bc3145b0f1170737c2b14147b3518bc92457
xuxiaoqiang <swust_xiaoqiangxu@163.com>
2021-06-22 19:07:25 +0800

    init memory leaks checker

--- clang/lib/Analysis/plugins/CheckerMalloc/MallocChecker.cpp
+++ clang/lib/Analysis/plugins/CheckerMalloc/MallocChecker.cpp
@@ -9,13 +9,17 @@ using namespace ento;
 
 namespace {
 
+typedef SmallVector<SymbolRef, 2> SymbolVector;
+
 enum Kind {
   Allocated,
   Released,
 };
 
-class MallocChecker : public Checker<check::PostCall, eval::Assume> {
+class MallocChecker : public Checker<check::PostCall, eval::Assume,
+                                     check::DeadSymbols, check::PointerEscape> {
   mutable std::unique_ptr<BugType> DoubleFreeBT;
+  mutable std::unique_ptr<BugType> LeakBT;
 
   CallDescription MallocFn;
   CallDescription FreeFn;
@@ -33,6 +37,12 @@ class MallocChecker : public Checker<check::PostCall, eval::Assume> {
                                 ProgramStateRef State) const;
 
   void reportDoubleFree(CheckerContext &C) const;
+  void reportLeaks(ArrayRef<SymbolRef> LeakedStreams, CheckerContext &C,
+                   ProgramStateRef State) const;
+
+  using LeakInfo = std::pair<const ExplodedNode *, const MemRegion *>;
+  static LeakInfo getAllocationSite(const ExplodedNode *N, SymbolRef Sym,
+                                    CheckerContext &C);
 
 public:
   MallocChecker() : MallocFn("malloc"), FreeFn("free"), ReallocFn("realloc") {}
@@ -40,6 +50,11 @@ public:
   void checkPostCall(const CallEvent &Call, CheckerContext &C) const;
   ProgramStateRef evalAssume(ProgramStateRef State, const SVal &Cond,
                              bool Assumption) const;
+  void checkDeadSymbols(SymbolReaper &SR, CheckerContext &C) const;
+  ProgramStateRef checkPointerEscape(ProgramStateRef State,
+                                     const InvalidatedSymbols &Escaped,
+                                     const CallEvent *Call,
+                                     PointerEscapeKind Kind) const;
 };
 
 } // anonymous namespace
@@ -83,12 +98,12 @@ MallocChecker::MallocMemAux(const CallEvent &Call, CheckerContext &C,
     return nullptr;
   }
 
-  SymbolRef Mem = Call.getReturnValue().getAsSymbol();
-  if (!Mem) {
+  SymbolRef Sym = Call.getReturnValue().getAsSymbol();
+  if (!Sym) {
     return nullptr;
   }
 
-  return State->set<RegionState>(Mem, Allocated);
+  return State->set<RegionState>(Sym, Allocated);
 }
 
 void MallocChecker::checkFree(const CallEvent &Call,
@@ -105,14 +120,14 @@ MallocChecker::FreeMemAux(const CallEvent &Call, CheckerContext &C,
     return nullptr;
   }
 
-  SymbolRef Mem = Call.getArgSVal(0).getAsSymbol();
-  if (!Mem) {
+  SymbolRef Sym = Call.getArgSVal(0).getAsSymbol();
+  if (!Sym) {
     return nullptr;
   }
 
-  const auto K = State->get<RegionState>(Mem);
+  const auto K = State->get<RegionState>(Sym);
   if (!K || *K == Allocated) {
-    return State->set<RegionState>(Mem, Released);
+    return State->set<RegionState>(Sym, Released);
   }
   if (*K == Released) {
     reportDoubleFree(C);
@@ -212,6 +227,141 @@ ProgramStateRef MallocChecker::evalAssume(ProgramStateRef State,
     }
   }
 
+  return State;
+}
+
+void MallocChecker::checkDeadSymbols(SymbolReaper &SR, CheckerContext &C) const {
+  auto isNotNull = [&](ProgramStateRef State, SymbolRef Sym) {
+    // Return true if a symbol is not NULL.
+    return !State->getConstraintManager().isNull(State, Sym).isConstrainedTrue();
+  };
+
+  ProgramStateRef State = C.getState();
+  SymbolVector LeakedSyms, DeadSyms;
+  for (const auto &TrackedRegion : State->get<RegionState>()) {
+    SymbolRef Sym = TrackedRegion.first;
+    if (SR.isLive(Sym)) {
+      continue;
+    }
+    DeadSyms.push_back(Sym);
+
+    if (TrackedRegion.second == Allocated && isNotNull(State, Sym)) {
+      LeakedSyms.push_back(Sym);
+    }
+  }
+
+  if (DeadSyms.empty()) {
+    return;
+  }
+
+  for (const auto &ReallocPair : State->get<ReallocPairs>()) {
+    if (SR.isDead(ReallocPair.first) || SR.isDead(ReallocPair.second)) {
+      State = State->remove<ReallocPairs>(ReallocPair.first);
+    }
+  }
+
+  if (!LeakedSyms.empty()) {
+    reportLeaks(LeakedSyms, C, State);
+  }
+
+  for (const auto &DeadSym : DeadSyms) {
+    State = State->remove<RegionState>(DeadSym);
+  }
+
+  C.addTransition(State);
+}
+
+MallocChecker::LeakInfo
+MallocChecker::getAllocationSite(const ExplodedNode *N, SymbolRef Sym,
+                                 CheckerContext &C) {
+  const LocationContext *LeakContext = N->getLocationContext();
+  const ExplodedNode *AllocNode = N;
+  const MemRegion *ReferenceRegion = nullptr;
+
+  while (N) {
+    ProgramStateRef State = N->getState();
+    if (!State->get<RegionState>(Sym)) {
+      break;
+    }
+
+    if (!ReferenceRegion) {
+      if (const MemRegion *MR = C.getLocationRegionIfPostStore(N)) {
+        SVal Val = State->getSVal(MR);
+        if (Val.getAsLocSymbol() == Sym) {
+          const VarRegion *VR = MR->getBaseRegion()->getAs<VarRegion>();
+          if (!VR || (VR->getStackFrame() == LeakContext->getStackFrame())) {
+            ReferenceRegion = MR;
+          }
+        }
+      }
+    }
+
+    const LocationContext *NContext = N->getLocationContext();
+    if (NContext == LeakContext || NContext->isParentOf(LeakContext)) {
+      AllocNode = N;
+    }
+    N = N->pred_empty() ? nullptr : *(N->pred_begin());
+  }
+
+  return LeakInfo(AllocNode, ReferenceRegion);
+}
+
+void MallocChecker::reportLeaks(ArrayRef<SymbolRef> LeakedSyms,
+                                CheckerContext &C,
+                                ProgramStateRef State) const {
+  ExplodedNode *N = C.generateNonFatalErrorNode(State);
+  if (!N) {
+    return;
+  }
+
+  if (!LeakBT) {
+    LeakBT.reset(new BugType(this, "Memory Leak", "Memory Error",
+      /*SuppressOnSink=*/true));
+  }
+
+  for (const auto &LeakedMem : LeakedSyms) {
+    PathDiagnosticLocation LocUsedForUniqueing;
+    const ExplodedNode *AllocNode = nullptr;
+    const MemRegion *Region = nullptr;
+    std::tie(AllocNode, Region) = getAllocationSite(N, LeakedMem, C);
+
+    const Stmt *AllocationStmt = AllocNode->getStmtForDiagnostics();
+    if (AllocationStmt) {
+      LocUsedForUniqueing = PathDiagnosticLocation::createBegin(AllocationStmt,
+        C.getSourceManager(), AllocNode->getLocationContext());
+    }
+
+    SmallString<200> buf;
+    llvm::raw_svector_ostream os(buf);
+    if (Region && Region->canPrintPretty()) {
+      os << "Potential leak of memory pointed to by ";
+      Region->printPretty(os);
+      os << " located at ";
+      LocUsedForUniqueing.asLocation().print(os, C.getSourceManager());
+    }
+    else {
+      os << "Potential memory leak";
+    }
+
+    auto R = std::make_unique<PathSensitiveBugReport>(*LeakBT, os.str(), N,
+      LocUsedForUniqueing, AllocNode->getLocationContext()->getDecl());
+    C.emitReport(std::move(R));
+  }
+}
+
+ProgramStateRef
+MallocChecker::checkPointerEscape(ProgramStateRef State,
+                                  const InvalidatedSymbols &Escaped,
+                                  const CallEvent *Call,
+                                  PointerEscapeKind Kind) const {
+  if (Kind == PSK_DirectEscapeOnCall && Call->isInSystemHeader()) {
+    return State;
+  }
+
+  for (SymbolRef Sym : Escaped) {
+    State = State->remove<RegionState>(Sym);
+    State = State->remove<ReallocPairs>(Sym);
+  }
   return State;
 }
 
