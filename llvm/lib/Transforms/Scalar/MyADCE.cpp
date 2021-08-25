31ec0471e07e70b4edc75331568aa52f2050faf0
xuxiaoqiang <swust_xiaoqiangxu@163.com>
2021-08-25 11:12:52 +0800

    remove not necessary include finle

--- llvm/lib/Transforms/Scalar/MyADCE.cpp
+++ llvm/lib/Transforms/Scalar/MyADCE.cpp
@@ -2,7 +2,6 @@
 #include "llvm/Transforms/Scalar.h"
 #include "llvm/IR/Function.h"
 #include "llvm/Pass.h"
-#include "llvm/Support/raw_ostream.h"
 #include "llvm/ADT/SmallVector.h"
 #include "llvm/ADT/SmallPtrSet.h"
 #include "llvm/IR/Instructions.h"
